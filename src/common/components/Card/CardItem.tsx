775f69329e984aff18fbf32077cb81248e8792e7
Duc Duy <ducduy2222001@gmail.com>
2024-04-24 00:43:49 +0700

    Build cardItem UI done

--- src/common/components/Card/CardItem.tsx
+++ src/common/components/Card/CardItem.tsx
@@ -1,21 +1,65 @@
 import React from "react";
-import { CardActionArea, Grid } from "@mui/material";
+import { Badge } from "antd";
 import CardMedia from "@mui/material/CardMedia";
+import { CardActionArea, Grid, IconButton } from "@mui/material";
+import { ShoppingBasket, RemoveRedEye } from "@mui/icons-material";
 
 import clothes from "../../../assets/image/clothes1.png";
+import styles from "./card.module.scss";
+
 const CardItem = () => {
   return (
     <>
-      <Grid item xs="auto">
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="330"
-            sx={{ width: "270px" }}
-            image={clothes}
-            alt="green iguana"
-          />
-        </CardActionArea>
+      <Grid
+        item
+        xs="auto"
+        container
+        direction="column"
+        justifyContent="space-between"
+        alignItems="flex-start"
+        sx={{ height: "430px" }}
+      >
+        <Badge.Ribbon text="Sell" placement="start">
+          <CardActionArea className={`${styles.card} `}>
+            <div
+              className={`${styles.btnsCard} flex flex-direction-column sssss`}
+            >
+              <IconButton
+                className={`${styles.iConButton}`}
+                children={<ShoppingBasket />}
+                disableRipple={true}
+                size="small"
+              />
+              <IconButton
+                className={`${styles.iConButton}`}
+                children={<RemoveRedEye />}
+                disableRipple={true}
+                size="small"
+              />
+            </div>
+            <CardMedia
+              component="img"
+              height="330"
+              sx={{ width: "270px" }}
+              image={clothes}
+              alt="green iguana"
+              className={`${styles.imgCard}`}
+            />
+          </CardActionArea>
+        </Badge.Ribbon>
+        <span
+          style={{
+            fontSize: "14px",
+            opacity: "0.5",
+            fontWeight: "600",
+          }}
+        >
+          MEN
+        </span>
+        <span className="font-l" style={{ fontWeight: "700" }}>
+          T-Shirt Name 1
+        </span>
+        <span className="font-m">$100</span>
       </Grid>
     </>
   );
