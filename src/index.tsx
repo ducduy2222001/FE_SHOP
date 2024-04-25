fac55e7ee1c9cd8c92dcd5c2ff2153f08cc246c9
Rory.Phan <rory.phan@avepoint.com>
2024-04-25 15:49:40 +0700

    restructure the file

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Home from "./views/components/Home/Home";
+import Home from "./views/components/home/Home";
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <Home />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
