a0d0df21c09327dd7d3cb071483fea87b056f2ee
Duc Duy <ducduy2222001@gmail.com>
2024-04-26 00:16:58 +0700

    update home page

--- src/views/components/Home/index.tsx
+++ src/views/components/Home/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { withLayout } from "../../../layout";
-
-import styles from "./home.module.scss";
 import { Grid } from "@mui/material";
+import { withLayout } from "../../../layout";
 import CardItem from "../../../common/components/card";
+import shoes2 from "../../../assets/image/shoes2.png";
+
+import "../../../assets/scss/common.scss";
+import styles from "./home.module.scss";
 
 const Home = () => {
   return (
-    <div className={`${styles.home}`}>
+    <div
+      className={`${styles.home} flex flex-direction-column flex-align-center`}
+    >
       <div className={`${styles.title}`}>Featured Products</div>
       <div style={{ width: "1190px", paddingBottom: "70px" }}>
         <Grid
@@ -24,7 +28,23 @@ const Home = () => {
           <CardItem />
         </Grid>
       </div>
-      <div className={`${styles.containA}`}></div>
+      <div
+        className={`${styles.containA} flex flex-direction-column flex-align-end flex-justify-center`}
+      >
+        <div className="flex flex-direction-column flex-align-center">
+          <img
+            src={shoes2}
+            alt=""
+            style={{
+              width: "500px",
+              height: "auto",
+            }}
+          />
+          <span style={{ fontSize: "40px", color: "var($color-white)" }}>
+            Explore Our Shoes Collection Now!
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
